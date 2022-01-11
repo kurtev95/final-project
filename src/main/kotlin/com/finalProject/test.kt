46eb9c490001f753d4679de968d2e201863920fe
dimitar <dkurtev@applica.com>
2022-01-11 11:41:33 +0200

    new docker-compose

---
+++ src/main/kotlin/com/finalProject/test.kt
@@ -0,0 +1,13 @@
+package com.finalProject
+
+import org.springframework.stereotype.Service
+
+@Service
+class test {
+
+
+    fun getKey():String{
+        val key = "tesaasd--dsaadsdsa--saddasads-dsa1e-23-213"
+        return key
+    }
+}
